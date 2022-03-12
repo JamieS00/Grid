61d483937a5fc9f8d284866fe72b7bfb0349f19e
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-03-12 14:36:41 -0500

    code for addRows

--- script.js
+++ script.js
@@ -55,36 +55,43 @@ function addR()
 
 
 
-// //Adds a column
-// function addC() {
-//     //alert("Clicked Add Col")
-//     let cols = document.getElementsByTagName("td");
-//     console.log(cols.length); 
-// }
+//Adds a column
+function addC() 
+{
+    //alert("Clicked Add Col")
+    let cols = document.getElementsByTagName("td");
+    console.log(cols.length); 
+}
 
-// //Removes a row
-// function removeR() {
-//     alert("Clicked Remove Row")
-// }
-// //Remove a column
-// function removeC() {
-//     alert("Clicked Remove Col")
-// }
+//Removes a row
+function removeR() 
+{
+    alert("Clicked Remove Row")
+}
+//Remove a column
+function removeC() 
+{
+    alert("Clicked Remove Col")
+}
 //sets global var for selected color
-function selected(){
+function selected()
+{
     colorSelected = document.getElementById("selectedID").value;
     console.log(colorSelected);
 }
 
-// function fill(){
-//     alert("Clicked Fill All")
-// }
+function fill()
+{
+    alert("Clicked Fill All")
+}
 
-// function clearAll(){
-//     alert("Clicked Clear All")
-// }
+function clearAll()
+{
+    alert("Clicked Clear All")
+}
 
-// function fillU(){
-//     alert("Clicked Fill All Uncolored")
-// }
+function fillU()
+{
+    alert("Clicked Fill All Uncolored")
+}
 
