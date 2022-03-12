4e724130e4ebbad32ae0c825956ead36e7ff27b5
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-03-12 13:16:04 -0500

    ...

--- script.js
+++ script.js
@@ -6,54 +6,60 @@ function addR()
     //alert("Clicked Add Row")
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
-    console.log(rows.length);
+    console.log(rows.length);////////////////////////////
+
     
-    if (rows.length === 0) {
-        let row = document.createElement("tr");
-        let col = document.createElement("td");
-        col.onclick = function (){
-            this.style.backgroundColor = colorSelected;
-        };
-        row.appendChild(col);
-        grid.appendChild(row);
+    
+    // if (rows.length === 0) 
+    // {
+    //     let row = document.createElement("tr");
+    //     let col = document.createElement("td");
+    //     col.onclick = function ()
+    //     {
+    //         this.style.backgroundColor = colorSelected;
+    //     };
+    //     row.appendChild(col);
+    //     grid.appendChild(row);
 
-    }
-}
-//Adds a column
-function addC() {
-    //alert("Clicked Add Col")
-    let cols = document.getElementsByTagName("td");
-    console.log(cols.length); 
+    // }
 }
 
-//Removes a row... probably thinking if the length != 0 then remove child -> removeChild()`
-function removeR() 
-{
-    alert("Clicked Remove Row")
-    if (rows.length != 0)
-    {
-        row.removeChild(col);
-        grid.removeChild(row);
-    }
-}
-//Remove a column
-function removeC() {
-    alert("Clicked Remove Col")
-}
-//sets global var for selected color
-function selected(){
-    colorSelected = document.getElementById("selectedID").value;
-    console.log(colorSelected);
-}
 
-function fill(){
-    alert("Clicked Fill All")
-}
+// //Adds a column
+// function addC() {
+//     //alert("Clicked Add Col")
+//     let cols = document.getElementsByTagName("td");
+//     console.log(cols.length); 
+// }
 
-function clearAll(){
-    alert("Clicked Clear All")
-}
+// //Removes a row... probably thinking if the length != 0 then remove child -> removeChild()`
+// function removeR() 
+// {
+//     alert("Clicked Remove Row")
+//     if (rows.length != 0)
+//     {
+//         row.removeChild(col);
+//         grid.removeChild(row);
+//     }
+// }
+// //Remove a column
+// function removeC() {
+//     alert("Clicked Remove Col")
+// }
+// //sets global var for selected color
+// function selected(){
+//     colorSelected = document.getElementById("selectedID").value;
+//     console.log(colorSelected);
+// }
 
-function fillU(){
-    alert("Clicked Fill All Uncolored")
-}
+// function fill(){
+//     alert("Clicked Fill All")
+// }
+
+// function clearAll(){
+//     alert("Clicked Clear All")
+// }
+
+// function fillU(){
+//     alert("Clicked Fill All Uncolored")
+// }
